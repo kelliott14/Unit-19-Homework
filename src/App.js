1fc611a266594712a06131dce800ed2e6f176a30
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-12-04 16:46:40 +1100

    scoring is working. End game is buggy

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from "./components/Navbar";
 import Game from "./components/Game";
 import './App.css';
+import GameCards from "./gamecard.json";
 
 
 
@@ -9,16 +10,57 @@ class App extends React.Component {
   state = {
     currentScore: 0,
     topScore: 0,
-    num: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    cards: GameCards,
+    pickedCards: []
   }
 
-  
+  scoreUpdate = () => {
+    this.setState({
+      currentScore: (this.state.currentScore + 10)
+    })
+  }
+
+  handleClick = id => {
+    //check if already picked, add to score
+    let check = this.state.pickedCards.includes(id) 
+    check ? this.endGame() : this.scoreUpdate()
+    //adds id to picked cards array
+    this.setState({pickedCards: [this.state.pickedCards, id]})
+
+
+    //shuffles game cards into a new order
+    var temp = this.state.cards
+    for (var i = temp.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var k = temp[i];
+      temp[i] = temp[j];
+      temp[j] = k;
+    }
+    this.setState({cards: temp})
+  }
+
+  endGame = () => {
+    if(this.state.currentScore > this.state.topScore) {
+      this.setState({topScore: this.state.currentScore}) 
+    }
+    this.setState({
+      currentScore: 0,
+      pickedCards: []
+    })
+  }
 
   render() {
     return (
       <div>
-      <Navbar />
-      <Game />
+      <Navbar currentScore={this.state.currentScore} topScore={this.state.topScore}/>
+      {this.state.cards.map(card => {
+        return <Game  
+                  handleClick={this.handleClick} 
+                  image={card.image} 
+                  key={card.id}
+                  id={card.id}
+              />  
+      })}
       </div>
     )
   }
